8b4c9f55891912927db06c706cffdf670d0cf862
obedm503 <obedm503@gmail.com>
2024-09-13 19:59:10 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://obedm.com",
-  integrations: [solid(), tailwind(), sitemap()],
+  integrations: [solid(), tailwind(), sitemap(), mdx()],
 });
